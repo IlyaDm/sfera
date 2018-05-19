adb28a0b68b1d320ee0b9d927051c76b7bc5416a
u <sferacoin@gmail.com>
2018-05-19 14:48:00 +0300

    preprod v1

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x13, 0xd2, 0x29, 0x11, 0x5f, 0x4f, 0x41, 0x4f, 0x5e, 0x45, 0x3d, 0x15, 0x1d, 0x21, 0x51, 0x1b } };
+}
