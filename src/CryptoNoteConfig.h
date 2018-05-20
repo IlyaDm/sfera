6ed730f177a8c636552ed361459919e7b9195d28
sferacoin <39261941+sferacoin@users.noreply.github.com>
2018-05-20 15:27:30 +0300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -95,7 +95,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   //"your_seed_ip1.com:17236",
-  "35.200.209.190:17236",
+  "35.200.190.16:17236",
 };
 
 struct CheckpointData {
